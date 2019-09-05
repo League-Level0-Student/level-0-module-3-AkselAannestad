9f7332783597a5c8ed695a4d96b3c2acdab4b296
League Student <league@iMac-18.attlocal.net>
2019-09-04 19:57:09 -0700

    HOROSCOPE
    Get 360 horoscoped m8

--- src/_04_are_you_happy/AreUHappy.java
+++ src/_04_are_you_happy/AreUHappy.java
@@ -1,5 +1,18 @@
 package _04_are_you_happy;
 
-public class AreUHappy {
+import javax.swing.JOptionPane;
 
-}
+public class AreUHappy {
+public static void main(String[] args) {
+	String one=JOptionPane.showInputDialog("Are you happy? (yes or no)");
+	String two = null; 
+	if (one.equals("no")) {two=JOptionPane.showInputDialog("Do you want to be happy?(yes or no)");}
+	else {JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");}
+	if (two.equals("yes")) {JOptionPane.showMessageDialog(null, "Change something");}
+	else {JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");}
+
+
+
+
+
+}}
